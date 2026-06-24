41a48e3a7d21b75d7882cdb7cdc57a6e3c102174
moarij <moarijmehmood54@gmail.com>
2026-06-24 14:21:09 -0700

    Remove before/after section from home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,6 @@
 import HeroSection from '@/components/home/HeroSection'
 import ServicesCarousel from '@/components/home/ServicesCarousel'
 import WhyChooseUs from '@/components/home/WhyChooseUs'
-import BeforeAfterSlider from '@/components/home/BeforeAfterSlider'
 import TestimonialsMarquee from '@/components/home/TestimonialsMarquee'
 import InstagramGrid from '@/components/home/InstagramGrid'
 import LoyaltyTeaser from '@/components/home/LoyaltyTeaser'
@@ -16,7 +15,6 @@ export default function HomePage() {
       <StatsBar />
       <ServicesCarousel />
       <WhyChooseUs />
-      <BeforeAfterSlider />
       <TestimonialsMarquee />
       <LoyaltyTeaser />
       <InstagramGrid />
